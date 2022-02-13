58dd8ee9cc24a96d631536c8833cf400496904c2
Shawan Mandal <shawan.sm049@gmail.com>
2022-02-13 17:00:49 +0530

    UI updates in note sharing page

--- src/pages/Shared.js
+++ src/pages/Shared.js
@@ -7,7 +7,7 @@ const Shared = (props) => {
     const { token } = props.match.params;
     const [note, setNote] = useState({})
     const [loading, setLoading] = useState(true)
-    const [error, setError] = useState({ message: '' })
+    const [error, setError] = useState(false)
     const headers = {
         "Content-Type" : "application/json",
         "accept": "*/*"
@@ -21,11 +21,11 @@ const Shared = (props) => {
                 data: {},
                 headers: headers
               }).then((resp) => {
-                    setNote({ ...note, title: resp.data[0].title, content: resp.data[0].content, author: resp.data[0].author })
+                    setNote({ ...note, ...resp.data[0] })
                     setLoading(false)
               })
               .catch((err) => {
-                  setError({ ...error, message: err.response ? err.response.data.message : err.message }) 
+                  setError(true) 
                   setLoading(false)
                 })
         }
@@ -33,31 +33,52 @@ const Shared = (props) => {
         // eslint-disable-next-line
     }, [])
 
+    const dateFormatter = (timestamp) => {
+        let date = new Date(timestamp)
+        return `${date.getDate()} ${date.toLocaleDateString(undefined, { month: "long" })}, ${date.getFullYear()}`
+    }
+
     return (
         <Fragment>
-            <div style={{ marginTop: '7rem' }} className='container'>
+            <div style={{ marginTop: '7rem', marginBottom: '4rem' }} className='container'>
             {!loading && note.title ?
             (
-                <Fragment>
+                <div style={{ borderRadius: '10px' }} className='m-1 p-3 border'>
                 <div style={{ textAlign: 'left' }}>
-                    <h4 style={{ fontWeight: 600 }} className="main-head">
+                    <h4 style={{ fontWeight: 600 }} className="shared-note-head mt-2">
                         {note.title}
                     </h4>
-                    <p>
+                    <p className='d-flex' style={{ justifyContent: 'space-between'}}>
                         <span>
                             {note.author ? (`by ${note.author.firstname} ${note.author.lastname} `) : ''}
+                        </span>
+                        <span style={{ fontWeight: 600 }}>
+                            {dateFormatter(note.updatedAt)} 
                         </span>
                     </p>
                 </div>
 
-                <div style={{ overflow: 'scroll' }} className="note-content mt-5" dangerouslySetInnerHTML={{ __html: note.content }}>
+                <div style={{ overflow: 'auto', textAlign: 'left' }} className="note-content mt-5" dangerouslySetInnerHTML={{ __html: note.content }}>
                 </div>
-                </Fragment>
-            ) : (
-                <h4 style={{ fontWeight: 600 }} className="main-head">
-                        {error.message}
-                </h4>
-            )}
+                </div>
+            ) : (error ? (
+                <div style={{ textAlign: 'left' }} className='row justify-content-center'>
+                    <div className="alert alert-danger" role="alert">
+                        <h4 style={{ fontWeight: 600 }} className="alert-heading mt-2 mb-3">Oh snap! You got an error!</h4>
+                        <p>The note that you requested was not found on this link. The possible reason for this error can be either: </p>
+                        <ul className='mt-2'>
+                            <li>
+                                The link shared doesn't exist or might be mistyped.
+                            </li>
+                            <li>
+                                The person sharing this link has disabled sharing of this note.
+                            </li>
+                        </ul>
+                        <hr/>
+                        <p className="mb-2">Please ask the person to re-share the note with you.</p>
+                    </div>
+                </div>
+            ) : '')}
             </div>
             <Backdrop
             sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
